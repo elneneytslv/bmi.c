c3db0d9a00e05dd9ffaf3dc8985a6576fd9cf7be
elneneytslv <eliancastella@gmail.com>
2026-06-14 22:43:50 -0300

    TP 03 Elian Castella

---
+++ bmi.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	
+	float Kg, h, bmi;
+	
+	printf("Ingrese el peso en kg: ");
+	scanf("%f", &Kg);
+	
+	printf("Ingrese la altura en mts: ");
+	scanf("%f", &h);
+	
+	bmi = Kg / (h * h);
+	
+	printf("\nSu índice de masa corporal es: %.2f\n\n", bmi);
+	
+	printf("  Indice   | Condicion  \n" );
+	printf("---------------------------\n");
+	printf("   <18.5   | Bajo peso\n");
+	printf("18.5 a 24.9| Normal\n");
+	printf("25.0 a 29.9| Sobrepeso\n");
+	printf("   >=30    | Obesidad\n");
+	
+	return 0;
+}
+
